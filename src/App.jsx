cb413a6a35236fe0d8ba14493b7f52ad977a2024
spider-guru <preciousgodwin559@gmail.com>
2024-05-22 19:54:54 +0100

    [chore]- added comment detailing future plans for the app on App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import { useState } from "react";
 // 3. enable ability to display movie synopsis via tooltip by hovering on search results
 // 4. enabling ability to switch themes and recall former theme state
 // 5. many more to come... stay tuned we are stil at the start position
+// 6. make provision for env variables.
+// 7. add few touches to the app to create a hint of originality
+// 8. create a more welcoming home page
 
 function App() {
 	let [query, setQuery] = useState("");
@@ -29,12 +32,10 @@ function App() {
 				setQueryResult={setQueryResult}
 			/>
 
-			<MovieCardContainer
-				isSearchFound={isSearchFound}
-				queryResult={queryResult}
-			/>
+			<MovieCardContainer isSearchFound={isSearchFound} queryResult={queryResult} />
 		</div>
 	);
 }
 
 export default App;
+
